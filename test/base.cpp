95a1b7bf3a304f76a15cdc225a918b8881a9335d
Hikki <mrmen.787@gmail.com>
2025-07-16 20:49:54 +0300

    Update Test

---
+++ test/base.cpp
@@ -0,0 +1,39 @@
+
+
+#include "iuic/context"
+
+template <iuic::style style = iuic::def_style> void button(auto &b) {
+  b.frame<style>([](auto &b) {
+    b.frame([](auto &b) {
+      //
+    });
+    b.frame([](auto &b) {
+      //
+    });
+    b.frame([](auto &b) {
+      //
+    });
+  });
+}
+
+int main() {
+  using namespace iuic;
+
+  context ctx;
+
+  ctx.make([](auto &b) {
+    b.frame([](auto &b) { return b.ret(); });
+
+    button(b);
+    button<{}>(b); // new style
+    button(b);
+
+    return b.ret();
+  });
+
+  ctx.event.key("A", KeyAction::Down);
+  ctx.event.key("B", KeyAction::Down);
+  ctx.event.pointer({22, 44});
+
+  return 0;
+}
