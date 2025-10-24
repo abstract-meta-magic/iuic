2307e98c1981c944c8e7238204e1664bdbaf7cc1
Hikki <mrmen.787@gmail.com>
2025-10-24 22:35:29 +0300

    Поправка .build...

--- .build/examples/CMakeFiles/button.dir/__/src/iuic-state.cpp.o.ddi.d
+++
@@ -1,2 +0,0 @@
-examples/CMakeFiles/button.dir/__/src/iuic-state.cpp.o.ddi: \
-  /home/hikki/job/cpp/iuic/project/src/iuic-state.cpp
--- .build/kit/cat-kit/CMakeFiles/cat_kit.dir/__/__/src/iuic-state.cpp.o.ddi.d
+++
@@ -1,2 +0,0 @@
-kit/cat-kit/CMakeFiles/cat_kit.dir/__/__/src/iuic-state.cpp.o.ddi: \
-  /home/hikki/job/cpp/iuic/project/src/iuic-state.cpp
