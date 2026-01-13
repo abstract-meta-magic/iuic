5381e504598143fe14aafc5141b6ae33c027e65c
Hikki <mrmen.787@gmail.com>
2026-01-13 21:19:27 +0300

    Simple license header.

--- src/iuic-ctx.cpp
+++ src/iuic-ctx.cpp
@@ -1,5 +1,6 @@
-// SPDX-FileCopyrightText: 2026 abstract-meta-magic and contributors
+// Copyright (c) 2026 abstract-meta-magic and contributors
 // SPDX-License-Identifier: Apache-2.0
+
 module iuic.core;
 import std;
 
--- src/iuic-text-font.cpp
+++ src/iuic-text-font.cpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2026 abstract-meta-magic and contributors
+// Copyright (c) 2026 abstract-meta-magic and contributors
 // SPDX-License-Identifier: Apache-2.0
 module iuic.core;
 import std;
